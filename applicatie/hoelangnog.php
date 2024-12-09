896cd4a0e68026921d596b1b77c546ca2318bf4b
Sanujen1999 <s.kumar4@student.han.nl>
2024-12-09 09:05:25 +0100

    php
    php

---
+++ applicatie/hoelangnog.php
@@ -0,0 +1,26 @@
+<?php
+
+$date1=date_create("now");
+$date2=date_create("05-12-2024");
+$diff=date_diff($date1,$date2);
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP voorbeeld</title>
+</head>
+<body>
+ 
+<?php
+echo 'Sinterklaas duurt nog '. $diff->format(" %a dagen en %h uren ") . '<br>';
+  
+?>
+
+
+</body>
+</html>
