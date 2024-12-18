61feb3f0506d1fba6e49e1b1eb222f183dbcb3eb
Sanujen1999 <s.kumar4@student.han.nl>
2024-12-18 11:45:38 +0100

    update php
    php

--- applicatie/componist.php
+++ applicatie/componist.php
@@ -1,47 +1,40 @@
 <?php
-require_once 'db_connectie.php';
 
+function sanitize($string){
+    return htmlspecialchars(strip_tags($string));
+}
+
+require_once 'db_connectie.php';
+$fouten = array();
 
 // 4 kolommen, dus ook 4 variabelen
-$componistId    = isset($_POST['componistId']) ? $_POST['componistId'] : '';
-$naam           = isset($_POST['naam']) ? $_POST['naam'] : '';
-$geboortedatum  = isset($_POST['geboortedatum']) ? $_POST['geboortedatum'] : '';
-$schoolId       = isset($_POST['schoolId']) ? $_POST['schoolId'] : '';
+if(isset($_POST['opslaan'])) {
+$componistId    =  sanitize($_POST['componistId']) ;
+$naam           =  sanitize($_POST['naam']);
+$geboortedatum  = sanitize($_POST['geboortedatum']) ;
+$schoolId       = sanitize($_POST['schoolId']) ;
 
+}
+function getSchoolIdSelectBox()
+{
  
+    $db = maakVerbinding();
+    $sql = 'select schoolID 
+            from componist';
+    $data = $db->query($sql);
 
-// Controleer niet verplichte velden
-if (empty($geboortedatum)) {
-    $geboortedatum = null;
-}
-// Controleer niet verplichte velden
-if (empty($schoolId)) {
-    $schoolId = null;
-}
+    $selectbox = '<select id="schoolID" name="schoolID">';
+    foreach($data as $rij)
+    {
+        $schoolID = $rij['schoolID'];
+        $selectbox .= "<option value=\"$schoolID\">$schoolID</option>";
+ 
+    }
+    $selectbox .= '</select>';
 
-$db = maakVerbinding();
-$sql = "INSERT INTO componist (componistId, naam, geboortedatum, schoolId) 
-VALUES (:componistId, :naam, :geboortedatum, :schoolId)";
-
-$query = $db->prepare($sql);
-$data_array = [
-    'componistId' => $componistId,
-    'naam' => $naam,
-    'geboortedatum' => $geboortedatum,
-    'schoolId' => $schoolId,
-];
-
-$succes = $query->execute($data_array);
-
-if ($succes) {
-    $melding = 'Gegevens zijn opgeslagen';
-}else{
-    $melding = 'Niet opgeslagen';
+    return $selectbox;
 }
 
-$fouten = [];
-// Controleer velden op geldigheid
-// componist id (not null, numeric)
 if (empty($componistId)) {
     $fouten[] = 'componistId is verplicht om in te vullen.';
 }
@@ -53,6 +46,15 @@ if (!is_numeric($componistId)) {
 // Naam (not null, text)
 if (empty($naam)) {
     $fouten[] = 'naam is verplicht om in te vullen.';
+}
+
+// Controleer niet verplichte velden
+if (empty($geboortedatum)) {
+    $geboortedatum = null;
+}
+// Controleer niet verplichte velden
+if (empty($schoolId)) {
+    $schoolId = null;
 }
 
 if (count($fouten) > 0) {
@@ -67,8 +69,30 @@ if (count($fouten) > 0) {
     $melding .= '</ul>';
 } else
 {
-    // ....... opslaan in db
+    $db = maakVerbinding();
+    $sql = "INSERT INTO componist (componistId, naam, geboortedatum, schoolId) 
+    VALUES (:componistId, :naam, :geboortedatum, :schoolId)";
+    
+    $query = $db->prepare($sql);
+    $data_array = [
+        'componistId' => $componistId,
+        'naam' => $naam,
+        'geboortedatum' => $geboortedatum,
+        'schoolId' => $schoolId,
+    ];
+    
+    $succes = $query->execute($data_array);
+
+    if ($succes) {
+        $melding = 'Gegevens zijn opgeslagen';
+    }else{
+        $melding = 'Niet opgeslagen';
+    }
 }
 
+
+
 echo $melding
+
 ?>
+<
