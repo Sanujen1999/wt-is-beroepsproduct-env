9888285ac0f503209bd8478111bbd7b70545ca7e
Sanujen1999 <s.kumar4@student.han.nl>
2025-01-08 11:42:46 +0100

    updated
    updated

--- applicatie/registratie.php
+++ applicatie/registratie.php
@@ -81,8 +81,8 @@ if (isset($_POST['registeren'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../wtisopdracht/CSS/normalize.css">
-    <link rel="stylesheet" href="../wtisopdracht/CSS/style.css">
+    <link rel="stylesheet" href="..CSS/normalize.css">
+    <link rel="stylesheet" href="..CSS/style.css">
     <title>Registratie</title>
 </head>
 
