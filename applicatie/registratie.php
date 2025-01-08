3fa4ea3c89b5b464b433fee4e1629fe4f749f5d9
Sanujen1999 <s.kumar4@student.han.nl>
2025-01-08 13:17:40 +0100

    updated
    updated

--- applicatie/registratie.php
+++ applicatie/registratie.php
@@ -1,5 +1,6 @@
 <?php
-require_once '../library/db_connectie.php';
+require_once '../applicatie/library/db_connectie.php';
+
 $melding = '';  // Initialisatie melding
 
 // Check of het formulier is ingediend
@@ -80,8 +81,8 @@ if (isset($_POST['registeren'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="..CSS/normalize.css">
-    <link rel="stylesheet" href="..CSS/style.css">
+    <link rel="stylesheet" href="../CSS/normalize.css">
+    <link rel="stylesheet" href="../CSS/style.css">
     <title>Registratie</title>
 </head>
 
